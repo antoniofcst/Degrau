0b94067eb334d19acfb135d20b90720e2ca7d6ad
Antônio Fernando <fernandocosta@alu.ufc.br>
2022-12-05 14:26:37 -0300

    Término da tela de postagem mais ajuste nas permissões

---
+++ app/src/main/java/com/degrau/activities/PostagemFragment.java
@@ -0,0 +1,66 @@
+package com.degrau.activities;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.degrau.R;
+
+public class PostagemFragment extends Fragment {
+
+    private Button buttonAbrirGaleria, buttonAbrirCamera;
+    private static final int SELECAO_CAMERA = 100;
+    private static final int SELECAO_GALERIA = 200;
+    private String[]  permissoesNecessarias = new String[] {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
+
+    public PostagemFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_postagem, container, false);
+
+        Permissao.validarPermissoes(permissoesNecessarias, getActivity(), 1);
+
+        buttonAbrirCamera = view.findViewById(R.id.buttonAbrirCamera);
+        buttonAbrirGaleria = view.findViewById(R.id.buttonAbrirGaleria);
+
+        buttonAbrirCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                 if(i.resolveActivity(getActivity().getPackageManager()) !=  null) {
+                     startActivityForResult(i, SELECAO_CAMERA);
+                 }
+            }
+        });
+
+        buttonAbrirGaleria.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent( Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI );
+                if(i.resolveActivity(getActivity().getPackageManager()) !=  null) {
+                    startActivityForResult(i, SELECAO_GALERIA);
+                }
+            }
+        });
+
+        return view;
+    }
+}
